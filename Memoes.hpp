069e4329bfe57824366353fa63cb9418bbb7ed8b
Michael L. Collard <collard@uakron.edu>
2021-11-15 07:32:17 -0500

    Initial commit

---
+++ Memoes.hpp
@@ -0,0 +1,15 @@
+/*
+    Memoes.hpp
+
+    A collection of memoes
+*/
+
+#ifndef INCLUDED_MEMOES_HPP
+#define INCLUDED_MEMOES_HPP
+
+class Memoes {
+public:
+    int size() const;
+};
+
+#endif
---
+++ MemoesTest.cpp
@@ -0,0 +1,19 @@
+/*
+    MemoesTest.cpp
+
+    Test program for Memoes
+*/
+
+#include "Memoes.hpp"
+#include <cassert>
+
+int main() {
+
+    {
+        Memoes Memoes;
+
+        assert(Memoes.size() == 0);
+    }
+
+    return 0;
+}
